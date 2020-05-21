225c3456f84e7c6141d5bbd191816dbffa3aa683
Michał Siatkowski <michal.siatkowski@cortb.pl>
2020-05-21 14:36:13 +0200

    lol no need docker

--- dev-deploy.sh
+++ dev-deploy.sh
@@ -1,8 +1,5 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-cp "Gemfile" "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart"
-cp "docker-compose.yml" "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart"
+rm -rf "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart"
 
-rm -rf "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart/_site"
-
-cp -r "_site" "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart"
+cp -r "./_site" "/run/user/1000/gvfs/smb-share:server=qsiata.local,share=web/logosart"
